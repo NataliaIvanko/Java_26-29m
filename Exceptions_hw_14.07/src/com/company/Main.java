c960dfb902413d3531a162de862a57a79dadf102
NataliaIvanko <natalie0505@yandex.ru>
2022-07-14 15:40:29 +0200

    Exceptions hw

---
+++ Exceptions_hw_14.07/src/com/company/Main.java
@@ -0,0 +1,59 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//Есть List<String>. Все элементы в нем должны быть строки-целые числа вида “231”, но некоторые данные повреждены (например, содержат символы).
+        // Необходимо преобразовать данный список в List<Integer>.
+        // При этом вывести на экран детализированный список «ошибок» т.е. в каких ячейках и
+        // какие значения не были преобразованы.
+
+        List<String> list =  Arrays.asList("234", "564", "5&6");
+        System.out.println(catchException(list));
+        System.out.println(getErrors(list));
+        int[] newArr = new int[10];
+
+
+        System.out.println(getSize(newArr));
+
+    }
+    public static List<String> catchException(List<String> list){
+        List<Integer> correctDataList = new ArrayList<>();
+        List<String> errorsList = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            try{
+                correctDataList.add(Integer.parseInt(list.get(i)));
+            }catch(NumberFormatException e){
+                errorsList.add(list.get(i)+ " is a wrong type");
+            }
+        }
+        return errorsList;
+    }
+    public static Map<Integer,String> getErrors(List<String> list) {
+        Map<Integer, String> errorsMap = new HashMap<>();
+        if (list == null) return errorsMap;
+
+        for (int i = 0; i < list.size(); i++) {
+            try {
+                Integer.parseInt(list.get(i));
+            }catch (NumberFormatException e){
+                errorsMap.put(i, list.get(i));
+            }
+        }
+
+        return errorsMap;
+    }
+    //2. Допустим, Ваш метод должен получить массив int размером 10x10 элементов.
+    // Необходимо, что бы ваш метод бросал IllegalArgumentException если размер переданного массива
+    // некорректен или вместо массива пришел null.
+
+    public static boolean getSize (int[] array) throws IllegalArgumentException{
+       // int[] newArr = new int[10];
+        if(array.length !=10  || array==null ){
+            throw new IllegalArgumentException("The size is not correct!");
+        }
+        return true;
+    }
+}
--- mapAB_hw/src/com/company/Main.java
+++ mapAB_hw/src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -20,13 +21,36 @@ public class Main {
         mapStr.put("f", "doing");
         System.out.println(concatAB(mapStr));
 
+
+        addSummaryValue(mapStr, List.of("a", "b", "c"));
+        System.out.println(mapStr);
+
     }
     public static Map<String, String>concatAB(Map<String, String>mapStr){
-        if(mapStr.containsKey("a") && mapStr.containsKey("b")){
+        if(mapStr != null && !mapStr.isEmpty()&& mapStr.containsKey("a") && mapStr.containsKey("b")){
             Map<String, String>mapAB = new HashMap<>();
             mapAB.put("ab", mapStr.get("a").concat(mapStr.get("b")));
             return mapAB;
         }
         return mapStr;
+    }
+    public static void concatAB1(Map<String, String>mapStr, String a, String b) {
+        if (mapStr != null && !mapStr.isEmpty() && mapStr.containsKey("a") && mapStr.containsKey("b")) {
+
+            mapStr.put(a+b, mapStr.get(a).concat(mapStr.get(b)));
+
+        }
+
+    }
+    public static void addSummaryValue(Map<String, String>map, List<String> keys){
+        if(map != null && !map.isEmpty()&& map.keySet().containsAll(keys)){
+            String resValue = "";
+            String resKey = "";
+            for(String key: keys){
+                resValue+=key;
+                resKey+=map.get(key);
+            }
+            map.put(resKey, resValue);
+        }
     }
 }
