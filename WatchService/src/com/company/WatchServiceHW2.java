bfcb8acce81fdc0b387b305d44be96d497fb316b
NataliaIvanko <natalie0505@yandex.ru>
2022-11-17 10:48:39 +0100

    WatchService

---
+++ WatchService/src/com/company/WatchServiceHW2.java
@@ -0,0 +1,77 @@
+package com.company;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class WatchServiceHW2 {
+
+    public static final String DIR_PATH = "D:\\TaskFolderTest1\\";
+    public static final String mainDirectory = "D:\\";
+
+    public static void main(String[] args) throws IOException {
+
+        moveFilesToAppropriateDir();
+
+    }
+
+    public static void moveFilesToAppropriateDir() throws IOException {
+        List<String> files = new ArrayList<>();
+        String place = "";
+        WatchService watchService = FileSystems.getDefault().newWatchService();
+        Path directory = Paths.get(DIR_PATH);
+        Path mainDir = Paths.get(mainDirectory);
+        WatchKey watchKey = directory.register(watchService, StandardWatchEventKinds.ENTRY_CREATE);
+
+        while (true) {
+
+            String command = getConsoleMessage("Enter a command: exit to exit, list to get a list");
+            for (WatchEvent<?> event : watchKey.pollEvents()) {
+                Path file = (Path) event.context();
+                try {
+                    Files.move(Path.of(directory + "\\" + file), Path.of(mainDir + "\\" + checkFileExtension(file.toString()) + "\\" + file));
+
+                    place = Path.of(mainDir + "\\" + checkFileExtension(file.toString()) + "\\" + file).toString();
+
+                } catch (FileSystemException e) {
+                    Files.createDirectory(Paths.get(mainDir + "\\" + checkFileExtension(file.toString())));//не успеваю переназвать, при создании ошибка файл существует в дест папке
+                    Files.move(Path.of(directory + "\\" + file), Path.of(mainDir + "\\" + checkFileExtension(file.toString()) + "\\" + file));
+
+                    place = Path.of(mainDir + "\\" + checkFileExtension(file.toString()) + "\\" + file).toString();
+                }
+            }
+            files.add(place);
+            Boolean exitMode = false;
+            switch (command) {
+                case "exit":
+                    exitMode = true;
+                    break;
+                case "list":
+
+                    files.forEach(System.out::println);
+                    break;
+                default:
+                    System.out.println("Invalid input");
+            }
+
+            if (exitMode) break;
+
+        }
+    }
+
+    public static String checkFileExtension(String name) {
+        String fileName = new File(name).getName();
+        int index = fileName.lastIndexOf(".");
+        return (index == -1) ? "" : fileName.substring(index + 1);
+    }
+
+    public static String getConsoleMessage(String caption) {
+        System.out.println(caption);
+        return new Scanner(System.in).nextLine();
+
+    }
+}
